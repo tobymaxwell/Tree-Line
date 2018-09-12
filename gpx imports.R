f3c6cfb228b063a22396e87c6f1462942e91199f
tobymaxwell <tmaxwell@uoregon.edu>
2018-09-12 08:18:32 -0700

    completed loop

---
+++ gpx imports.R
@@ -0,0 +1,18 @@
+#kml to csv
+library(rgdal)
+library(raster)
+library(maptools)
+library(tools)
+setwd("/Users/tobymaxwell/OneDrive - University Of Oregon/Oregon/Nat Geo/GPX files/")
+files<-list.files(pattern=".gpx")
+files
+k<-NA
+m<-NA
+l<-NA
+for(i in files){
+  k<-readOGR(i)
+  l<-data.frame(subset(k, select=c('ele', "name")))
+  m<-merge(l,m, all=T)
+}
+m
+tail(m, 50)
